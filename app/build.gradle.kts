9d4d02e747eba0edf1531b8fc3d74c8ca2d02083
Sasha <samuray150305@gmail.com>
2024-06-23 15:01:58 +0300

    DayWeekAdapter но надо менять внутрянку под наш проект

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.navigation.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/smg/mironov/ksuschedule/Adapters/DayWeekAdapter.java
+++ app/src/main/java/smg/mironov/ksuschedule/Adapters/DayWeekAdapter.java
@@ -1,4 +1,53 @@
 package smg.mironov.ksuschedule.Adapters;
 
-public class DayWeekAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class StateAdapter  extends RecyclerView.Adapter<StateAdapter.ViewHolder>{
+
+    private final LayoutInflater inflater;
+    private final List<State> states;
+
+    StateAdapter(Context context, List<State> states) {
+        this.states = states;
+        this.inflater = LayoutInflater.from(context);
+    }
+    @Override
+    public StateAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = inflater.inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(StateAdapter.ViewHolder holder, int position) {
+        State state = states.get(position);
+        holder.flagView.setImageResource(state.getFlagResource());
+        holder.nameView.setText(state.getName());
+        holder.capitalView.setText(state.getCapital());
+    }
+
+    @Override
+    public int getItemCount() {
+        return states.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        final ImageView flagView;
+        final TextView nameView, capitalView;
+        ViewHolder(View view){
+            super(view);
+            flagView = view.findViewById(R.id.flag);
+            nameView = view.findViewById(R.id.name);
+            capitalView = view.findViewById(R.id.capital);
+        }
+    }
 }
